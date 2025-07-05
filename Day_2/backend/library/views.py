43a66835e65e3c7f3ca2fd1fecaf6072586e264e
DeveloperTunc <tunc.devstack@gmail.com>
2025-07-05 04:35:16 +0300

    Django boilerplate code

---
+++ Day_2/backend/library/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+
+# Create your views here.
+
+def main(request):
+    return HttpResponse("Main Page")
+
+def add_book(request):
+    pass
+def delete_book(request):
+    pass
